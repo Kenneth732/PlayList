396f4d59d1f48b839ba7468ca1e3711567537ef7
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-08 07:19:37 +0300

    initi....function use to apend sing-item and class btn to be invoke when are clicked

--- static/js/app.js
+++ static/js/app.js
@@ -1,32 +1,34 @@
+
+
 // script.js
 const playlist = [
-    {
-      title: "Song 1",
-      artist: "Artist 1",
-      file: "/assets/LinkinPark.mp3"
-    },
-    {
-      title: "Song 2",
-      artist: "Artist 2",
-      file: "/assets/y2mate.com - Simple Plan  Addicted Official Video (1).mp3"
-    },
-    // Add more songs to the playlist array
+  {
+    title: "Song 1",
+    image: "images/p3.jpg",
+    artist: "Artist 1",
+    file: "/assets/LinkinPark.mp3"
+  },
+  {
+    title: "Song 2",
+    artist: "Artist 2",
+    image: "images/p2.png",
+    file: "/assets/y2mate.com - Simple Plan  Addicted Official Video (1).mp3"
+  },
+  // Add more songs to the playlist array
 ];
 
-const songList = document.querySelector('#song-list');
-const playButton = document.querySelector('#play-button');
-const pauseButton = document.querySelector('#pause-button');
-const nextButton = document.querySelector('#next-button');
-const previousButton = document.querySelector('#previous-button');
-const volumeRange = document.querySelector('#volume-range');
+const songList = document.getElementById("song-list");
+const playButton = document.getElementById("play-button");
+const pauseButton = document.getElementById("pause-button");
+const nextButton = document.getElementById("next-button");
+const previousButton = document.getElementById("previous-button");
+const volumeRange = document.getElementById("volume-range");
 
 let currentSongIndex = 0;
 let audio = new Audio();
 
-function loadSong(songIndex){
-    const song = playlist[songIndex]
-    audio.src = song.file;
-    audio.load()
+function loadSong(songIndex) {
+  const song = playlist[songIndex];
+  audio.src = song.file;
+  audio.load();
 }
-
-function playSong()
